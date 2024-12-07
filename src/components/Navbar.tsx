01a4a79fa2924fac0586706645ba603645dd5617
SadafKhalidSK <148052407+SadafKhalidSK@users.noreply.github.com>
2024-12-08 01:01:39 +0500

    my hacakathon

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+
+        
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="bg-tealCustom p-4">
+        <div className="container mx-auto flex justify-between item-center">
+        <div className="text-red-700 text-xl font-semibold">
+
+            NEXT_JS
+            </div>
+            <div className="flex space-x-12 font-bold text-lg">
+<a href="/" className="text-white">Home</a>
+<a href="/" className="text-white">About Us</a>
+
+<a href="/product" className="text-white">product</a>
+
+<a href="/" className="text-white">Service</a>
+</div>
+<div><button
+  type="submit"
+  className= "bg-white text-tealCustom px-4 py-1 rounded-lg font-bold">submit</button>
+</div>
+
+            </div>
+      </nav>
+    </div>
+  );
+}
+ 
+
+
